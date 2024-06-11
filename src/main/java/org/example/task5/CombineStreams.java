b504279a66e6c3a4e6966dd6c28aea9c2821ee4c
ruslana <ruslana.prus@ukr.net>
2024-06-11 21:19:32 +0200

    task 5 alternative solutions

--- src/main/java/org/example/task5/CombineStreams.java
+++ src/main/java/org/example/task5/CombineStreams.java
@@ -4,7 +4,9 @@
 package org.example.task5;
 
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -44,5 +46,61 @@ public class CombineStreams {
                 })
                 .takeWhile(pair -> pair != null)
                 .flatMap(pair -> pair);
+    }
+
+    //Alternative solutions:
+
+    public static <T> Stream<T> zipWithOptional(Stream<T> first, Stream<T> second) {
+        Objects.requireNonNull(first);
+        Objects.requireNonNull(second);
+
+        Iterator<T> iterator1 = first.iterator();
+        Iterator<T> iterator2 = second.iterator();
+
+        class Pair {
+            final Optional<T> firstElem;
+            final Optional<T> secondElem;
+
+            Pair(Optional<T> firstElem, Optional<T> secondElem) {
+                this.firstElem = firstElem;
+                this.secondElem = secondElem;
+            }
+
+            boolean hasBoth() {
+                return firstElem.isPresent() && secondElem.isPresent();
+            }
+        }
+
+        Pair initialPair = new Pair(
+                iterator1.hasNext() ? Optional.of(iterator1.next()) : Optional.empty(),
+                iterator2.hasNext() ? Optional.of(iterator2.next()) : Optional.empty()
+        );
+
+        return Stream.iterate(initialPair, pair -> {
+                    Optional<T> nextFirst = iterator1.hasNext() ? Optional.of(iterator1.next()) : Optional.empty();
+                    Optional<T> nextSecond = iterator2.hasNext() ? Optional.of(iterator2.next()) : Optional.empty();
+                    return new Pair(nextFirst, nextSecond);
+                })
+                .takeWhile(Pair::hasBoth)
+                .flatMap(pair -> Stream.of(pair.firstElem.get(), pair.secondElem.get()));
+    }
+
+    public static <T, U, R> Stream<R> zipBiFunction(Stream<T> first, Stream<U> second, BiFunction<T, U, R> zipper) {
+        Iterator<T> firstIterator = first.iterator();
+        Iterator<U> secondIterator = second.iterator();
+
+        Iterable<R> iterable = () -> new Iterator<R>() {
+            @Override
+            public boolean hasNext() {
+                return firstIterator.hasNext() && secondIterator.hasNext();
+            }
+
+            @Override
+            public R next() {
+                return zipper.apply(firstIterator.next(), secondIterator.next());
+            }
+        };
+
+        return StreamSupport.stream(iterable.spliterator(), false);
     }
 }
